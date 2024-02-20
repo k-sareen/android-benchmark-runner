5f5e0f85012fe6465f12f9d4b2d5192288bcd2cf
Kunal Sareen <kunal.sareen@anu.edu.au>
2024-02-21 00:55:51 +1100

    Make Instagram more stable

--- app/src/main/java/org/anu/benchmarkrunner/bms/InstagramScrollTest.java
+++ app/src/main/java/org/anu/benchmarkrunner/bms/InstagramScrollTest.java
@@ -6,6 +6,8 @@ import android.graphics.Rect;
 import android.util.Log;
 
 import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.BySelector;
+import androidx.test.uiautomator.StaleObjectException;
 import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.Until;
 
@@ -19,12 +21,9 @@ public class InstagramScrollTest extends Benchmark {
     static String PACKAGE_NAME = "com.instagram.android";
     static String ACTIVITY_NAME = "com.instagram.android.activity.MainTabActivity";
     static String MUTE_BUTTON = "com.instagram.android:id/indicator";
-    static String POST = "com.instagram.android:id/(carousel_video_)?media_group";
-    static String POST_HEADER = "com.instagram.android:id/top_legibility_gradient";
-    static String PROFILE_HEADER = "com.instagram.android:id/media_header_location";
-    static String PROFILE_NAME = "com.instagram.android:id/row_feed_photo_profile_name";
-    static String REFRESHABLE_CONTAINER = "com.instagram.android:id/refreshable_container";
-    static String SUGGESTED_FOR_YOU_HEADER = "com.instagram.android:id/row_feed_topic_label";
+    static String POST = "com.instagram.android:id/(((carousel_(video_)?)?media_group)|carousel_image)";
+    static String REELS_LIST = "reels_tray_container";
+    static BySelector POST_SELECTOR = By.res(Pattern.compile(POST)).desc(Pattern.compile(".*(likes|Liked).*"));
 
     String previousPostDesc = "";
     boolean muteButtonFound = false;
@@ -37,35 +36,35 @@ public class InstagramScrollTest extends Benchmark {
     @Override
     public boolean iterate() {
         try {
-            boolean found = device.wait(Until.hasObject(By.res(SUGGESTED_FOR_YOU_HEADER)), 5000);
+            boolean found = device.wait(Until.hasObject(By.desc(REELS_LIST)), 5000);
             if (!found) {
                 Log.i(LOG_TAG, "Main page did not load in time");
                 return false;
             }
 
-            device.swipe(deviceWidth / 2, 50 * deviceHeight / 100,
-                    deviceWidth / 2, 70 * deviceHeight / 100, 70);
-            Thread.sleep(2500);
+            device.swipe(deviceWidth / 2, 40 * deviceHeight / 100,
+                    deviceWidth / 2, 70 * deviceHeight / 100, 50);
+            Thread.sleep(2750);
 
-            found = device.wait(Until.hasObject(By.res(SUGGESTED_FOR_YOU_HEADER)), 5000);
+            found = device.wait(Until.hasObject(By.desc(REELS_LIST)), 5000);
             if (!found) {
                 Log.i(LOG_TAG, "Refresh did not finish in time");
                 return false;
             }
 
-            UiObject2 post = device.findObject(By.res(Pattern.compile(POST)));
+            UiObject2 post = device.findObject(POST_SELECTOR);
             Rect postBounds = post.getVisibleBounds();
             device.swipe(deviceWidth / 2, postBounds.bottom - deviceHeight / 100,
                     deviceWidth / 2, 40 * deviceHeight / 100, 35);
             device.waitForIdle();
-            Thread.sleep(750);
+            Thread.sleep(250);
 
             found = device.wait(Until.hasObject(By.res(MUTE_BUTTON)), 2000);
             if (found) {
                 muteButtonFound = true;
                 UiObject2 muteButton = device.findObject(By.res(MUTE_BUTTON));
                 muteButton.click();
-                Thread.sleep(750);
+                Thread.sleep(250);
             }
 
             for (int i = 0; i < 9; i++) {
@@ -94,7 +93,7 @@ public class InstagramScrollTest extends Benchmark {
     boolean scrollPost() throws InterruptedException {
         boolean found;
         UiObject2 post;
-        List<UiObject2> posts = device.wait(Until.findObjects(By.res(Pattern.compile(POST))), 2000);
+        List<UiObject2> posts = device.wait(Until.findObjects(POST_SELECTOR), 2000);
         if (posts == null || posts.size() == 0) {
             Log.i(LOG_TAG, "No posts visible");
             return false;
@@ -104,18 +103,23 @@ public class InstagramScrollTest extends Benchmark {
             post = posts.get(1);
         }
 
-        String postDesc = post.getContentDescription();
-        if (postDesc == null) {
-            Log.i(LOG_TAG, "Could not get post content description");
-            return false;
-        }
+//        String postDesc = post.getContentDescription();
+//        if (postDesc == null) {
+//            Log.i(LOG_TAG, "Could not get post content description");
+//            return false;
+//        }
 
-        Rect postBounds = post.getVisibleBounds();
-        int start = posts.size() == 1 ? postBounds.bottom - deviceHeight / 100 : postBounds.top;
-        device.swipe(deviceWidth / 2, start,
-                deviceWidth / 2, 8 * deviceHeight / 100, 40);
+        try {
+            Rect postBounds = post.getVisibleBounds();
+            int start = posts.size() == 1 ? postBounds.bottom - deviceHeight / 100 : postBounds.top;
+            device.swipe(deviceWidth / 2, start,
+                    deviceWidth / 2, 8 * deviceHeight / 100, 40);
+        } catch (StaleObjectException ignored) {
+            device.swipe(deviceWidth / 2, 70 * deviceHeight / 100,
+                    deviceWidth / 2, 30 * deviceHeight / 100, 40);
+        }
         device.waitForIdle();
-        Thread.sleep(750);
+        Thread.sleep(250);
 
         if (!muteButtonFound) {
             found = device.wait(Until.hasObject(By.res(MUTE_BUTTON)), 2000);
@@ -123,20 +127,22 @@ public class InstagramScrollTest extends Benchmark {
                 muteButtonFound = true;
                 UiObject2 muteButton = device.findObject(By.res(MUTE_BUTTON));
                 muteButton.click();
-                Thread.sleep(750);
+                Thread.sleep(250);
             }
         }
 
-        if (previousPostDesc.equals(postDesc)) {
-            Log.i(LOG_TAG, "Could not display new post");
-            return false;
-        } else {
-            previousPostDesc = postDesc;
-        }
+//        if (previousPostDesc.equals(postDesc)) {
+//            Log.i(LOG_TAG, "Could not display new post");
+//            Log.i(LOG_TAG, previousPostDesc);
+//            Log.i(LOG_TAG, postDesc);
+//            return false;
+//        } else {
+//            previousPostDesc = postDesc;
+//        }
 
-        found = device.hasObject(By.res(Pattern.compile(POST)));
+        found = device.hasObject(By.res("android:id/list"));
         if (!found) {
-            Log.i(LOG_TAG, "Could not find post after scrolling");
+            Log.i(LOG_TAG, "Could not find posts after scrolling");
             return false;
         }
 
