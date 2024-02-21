8cbd746626f21a45cd9cf5f8243ff6c6b065e3e2
Kunal Sareen <kunal.sareen@anu.edu.au>
2024-02-21 16:18:21 +1100

    Add benchmark for Google Photos

---
+++ app/src/main/java/org/anu/benchmarkrunner/bms/PhotosScrollTest.java
@@ -0,0 +1,104 @@
+package org.anu.benchmarkrunner.bms;
+
+import static org.anu.benchmarkrunner.BenchmarkRunner.LOG_TAG;
+
+import android.util.Log;
+
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiObject2;
+import androidx.test.uiautomator.Until;
+
+import org.anu.benchmarkrunner.Benchmark;
+
+import java.io.PrintStream;
+
+public class PhotosScrollTest extends Benchmark {
+    static String PACKAGE_NAME = "com.google.android.apps.photos";
+    static String ACTIVITY_NAME = "com.google.android.apps.photos.home.HomeActivity";
+    static String LIBRARY_TAB = "com.google.android.apps.photos:id/tab_library";
+    static String PHOTO_CONTAINER = "com.google.android.apps.photos:id/photos_photofragment_components_background_photo_view";
+
+    public PhotosScrollTest(PrintStream writer) {
+        super(PACKAGE_NAME, ACTIVITY_NAME, writer);
+    }
+
+    @Override
+    public boolean iterate() {
+        try {
+            UiObject2 libraryButton = device.wait(Until.findObject(By.res(LIBRARY_TAB)), 6000);
+            if (libraryButton == null) {
+                Log.i(LOG_TAG, "Main page did not load in time");
+                return false;
+            }
+
+            libraryButton.click();
+            device.waitForIdle();
+
+            UiObject2 downloadGallery = device.wait(Until.findObject(By.text("Download")), 6000);
+            if (downloadGallery == null) {
+                Log.i(LOG_TAG, "Could not find downloads library");
+                return false;
+            }
+
+            downloadGallery.click();
+            device.waitForIdle();
+
+            UiObject2 photo = device.wait(Until.findObject(
+                    By.clazz("android.widget.ImageView").descContains("taken on")), 8000);
+            if (photo == null) {
+                Log.i(LOG_TAG, "Could not find picture");
+                return false;
+            }
+
+            photo.click();
+            device.waitForIdle();
+
+            photo = device.wait(Until.findObject(By.res(PHOTO_CONTAINER)), 6000);
+            if (photo == null) {
+                Log.i(LOG_TAG, "Could not find fullscreen picture");
+                return false;
+            }
+
+            photo.pinchOpen(0.6f);
+            device.waitForIdle();
+            photo.pinchOpen(0.5f);
+            device.waitForIdle();
+            Thread.sleep(1000);
+
+            photo.click();
+            Thread.sleep(50);
+            photo.click();
+            device.waitForIdle();
+            Thread.sleep(500);
+
+            for (int i = 0; i < 5; i++) {
+                boolean found = device.wait(Until.hasObject(By.res(PHOTO_CONTAINER)), 6000);
+                if (!found) {
+                    Log.i(LOG_TAG, "Could not find fullscreen picture");
+                    return false;
+                }
+
+                device.swipe(70 * deviceWidth / 100, deviceHeight / 2,
+                        30 * deviceWidth / 100, deviceHeight / 2, 15);
+                device.waitForIdle();
+                Thread.sleep(500);
+            }
+
+            device.pressBack();
+            device.waitForIdle();
+            device.pressBack();
+            device.waitForIdle();
+
+            downloadGallery = device.wait(Until.findObject(By.text("Download")), 6000);
+            if (downloadGallery == null) {
+                Log.i(LOG_TAG, "Did not return to downloads library");
+                return false;
+            }
+
+            return true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return false;
+        }
+    }
+}
